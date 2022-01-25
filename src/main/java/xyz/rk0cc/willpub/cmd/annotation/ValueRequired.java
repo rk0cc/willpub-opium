100dd81cefe938dcea77842aaa3afaf77131202a
Cyrus Chan <cyruschan1212@gmail.com>
2022-01-25 10:47:15 +0800

    Incorrect design

--- src/main/java/xyz/rk0cc/willpub/cmd/annotation/ValueRequired.java
+++
@@ -1,9 +0,0 @@
-package xyz.rk0cc.willpub.cmd.annotation;
-
-import java.lang.annotation.*;
-
-@Documented
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-public @interface ValueRequired {
-}
