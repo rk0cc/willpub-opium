59a72f4a135f75ed77b88e21e20e35d65d466ae1
Cyrus Chan <cyruschan1212@gmail.com>
2022-02-05 16:05:01 +0800

    Add exception's package info

---
+++ src/main/java/xyz/rk0cc/willpub/exceptions/cmd/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides all exception needed for throwing related {@link java.lang.Exception} under {@link xyz.rk0cc.willpub.cmd}.
+ *
+ * @since 1.0.0
+ */
+package xyz.rk0cc.willpub.exceptions.cmd;
